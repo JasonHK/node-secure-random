2f3d1f0f7e86e8654c5ab6607252e975fe915685
Jason Kwok <JasonHK@users.noreply.github.com>
2020-03-16 00:37:16 +0800

    docs: added TSDoc

--- src/output-type.ts
+++ src/output-type.ts
@@ -1,9 +1,37 @@
 "use strict";
 
+/**
+ * AN enum consists of the options of available output types.
+ * 
+ * @since 0.0.1
+ */
 export const enum OutputType
 {
+    /**
+     * Set the output type as `Array`.
+     * 
+     * @since 0.0.1
+     */
     ARRAY        = "array",
+
+    /**
+     * Set the output type as `ArrayBuffer`.
+     * 
+     * @since 0.0.1
+     */
     ARRAY_BUFFER = "arraybuffer",
+
+    /**
+     * Set the output type as `Buffer`.
+     * 
+     * @since 0.0.1
+     */
     BUFFER       = "buffer",
+
+    /**
+     * Set the output type as `Uint8Array`.
+     * 
+     * @since 0.0.1
+     */
     UINT8_ARRAY  = "uint8array",
 }
