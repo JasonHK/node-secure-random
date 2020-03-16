58a91fa460e3628e1a02f9a08ec4d0150049189b
Jason Kwok <JasonHK@users.noreply.github.com>
2020-03-16 15:53:56 +0800

    docs: added TSDoc

--- src/validators/validate-number.ts
+++ src/validators/validate-number.ts
@@ -2,6 +2,13 @@
 
 import isNumber from "lodash.isnumber";
 
+/**
+ * Validates whether the payload is a number or not.
+ * 
+ * @hidden
+ * 
+ * @param payload An object to be determined.
+ */
 export function validateNumber(payload: unknown, name: string): asserts payload is number
 {
     if (!isNumber(payload))
--- src/validators/validate-string.ts
+++ src/validators/validate-string.ts
@@ -2,6 +2,13 @@
 
 import isString from "lodash.isstring";
 
+/**
+ * Validates whether the payload is a string or not.
+ * 
+ * @hidden
+ * 
+ * @param payload An object to be determined.
+ */
 export function validateString(payload: unknown, name: string): asserts payload is string
 {
     if (!isString(payload))
